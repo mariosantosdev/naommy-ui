0b6c3d99c3b70e0f5d5d1ff9ef4354fb0fbf0a87
Mário Santos <mariodev7@gmail.com>
2023-03-22 19:43:07 -0300

    create button component and docs

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { ArrowRight } from 'phosphor-react'
+import { Button, ButtonProps } from '@naommyUI/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Edit',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Next step
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
