71f41b646978ec2ce7afd74a330ba9d94d5e0433
Mário Santos <mariodev7@gmail.com>
2023-03-22 19:59:36 -0300

    create textarea component and docs

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Box, Text, TextArea, TextAreaProps } from '@naommyUI/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/TextArea',
+  component: TextArea,
+  decorators: [
+    (Story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+      >
+        <Text size="sm">Content</Text>
+        {Story()}
+      </Box>
+    ),
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Write something...',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+    placeholder: 'Disabled',
+  },
+}
